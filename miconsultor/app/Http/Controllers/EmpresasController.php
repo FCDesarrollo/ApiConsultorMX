ca4caab12987bfc723ea3b19a8b2ba623600f8f0
Alejandro Paez <46059841+AlejadroPaez@users.noreply.github.com>
2019-03-26 10:07:39 -0700

    base de datos por empresa

--- miconsultor/app/Http/Controllers/EmpresasController.php
+++ miconsultor/app/Http/Controllers/EmpresasController.php
@@ -139,16 +139,54 @@ class EmpresasController extends Controller
             tipopermiso INT(11) DEFAULT NULL
           ) ENGINE=INNODB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
         DB::statement($menupermis); 
-        
-        /*$submenupermis = "create table if not exists mc_submenupermis (
+
+        $userprofile = "create table if not exists mc_userprofile (
+            id int(11) NOT NULL,
+            idusuario int(11) DEFAULT NULL,
+            idperfil int(11) DEFAULT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
+          DB::statement($userprofile); 
+
+          $usermod = "create table if not exists mc_usermod (
+            id INT(11) NOT NULL,
+            idusuario INT(11) DEFAULT NULL,
+            idperfil INT(11) DEFAULT NULL,
+            idmodulo INT(11) DEFAULT NULL,
+            tipopermiso INT(11) DEFAULT NULL
+          ) ENGINE=INNODB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+          ";
+          DB::statement($usermod); 
+
+          $usermenu = "create table if not exists mc_usermenu (
+            id INT(11) NOT NULL,
+            idusuario INT(11) NOT NULL,
+            idperfil INT(11) NOT NULL,
+            idmodulo INT(11) NOT NULL,
+            idmenu INT(11) NOT NULL,
+            tipopermiso INT(11) NOT NULL
+          ) ENGINE=INNODB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
+          DB::statement($usermenu); 
+
+          $usersubmenu = "create table if not exists mc_usersubmenu (
+            id INT(11) NOT NULL,
+            idusuario INT(11) NOT NULL,
+            idperfil INT(11) NOT NULL,
+            idmenu INT(11) NOT NULL,
+            idsubmenu INT(11) NOT NULL,
+            tipopermiso INT(11) NOT NULL,
+            notificaciones INT(11) DEFAULT '0'
+          ) ENGINE=INNODB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
+          DB::statement($usersubmenu); 
+
+        $submenupermis = "create table if not exists mc_submenupermis (
             id INT(11) NOT NULL,
             idperfil INT(11) DEFAULT NULL,
             idmenu INT(11) DEFAULT NULL,    
             idsubmenu INT(11) DEFAULT NULL,
             tipopermiso INT(11) DEFAULT NULL,
-            notificaciones` INT(11) DEFAULT '0'
+            notificaciones INT(11) DEFAULT '0'
           ) ENGINE=INNODB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
-        DB::statement($submenupermis); */
+        DB::statement($submenupermis);
         
         $mc_profiles = "insert ".$empresaBD.".mc_profiles SELECT * FROM dublockc_MCGenerales.mc1006;";
         DB::statement($mc_profiles);
@@ -159,8 +197,8 @@ class EmpresasController extends Controller
         $mc_profiles = "insert ".$empresaBD.".mc_menupermis SELECT * FROM dublockc_MCGenerales.mc1008;";
         DB::statement($mc_profiles);
         
-        /*$mc_profiles = "insert ".$empresaBD.".mc_submenupermis SELECT * FROM dublockc_MCGenerales.mc1009;";
-        DB::statement($mc_profiles);*/
+        $mc_profiles = "insert ".$empresaBD.".mc_submenupermis SELECT * FROM dublockc_MCGenerales.mc1009;";
+        DB::statement($mc_profiles);
 
        return 1;
     }

3cc788e3ae29c5b51d67d81c0149b9df03772786
Enrique Aramburo <enrique_shady@hotmail.com>
2019-10-18 10:10:31 -0600

    Update web.php

--- miconsultor/routes/web.php
+++ miconsultor/routes/web.php
@@ -113,10 +113,10 @@ Route::get('PermisoSubMenusPerfil', 'GeneralesController@PermisoSubMenusPerfil')
 
 
 //ARCHIVOS EMPRESA - ALMACEN DIGITAL
-Route::get('RubrosGen', 'ConsumoController@RubrosGen');
-Route::get('CatSucursales', 'ConsumoController@CatSucursales');
+Route::post('RubrosGen', 'ConsumoController@RubrosGen');
+Route::post('CatSucursales', 'ConsumoController@CatSucursales');
 Route::post('AlmCargaArchivos', 'ConsumoController@AlmCargaArchivos');
-Route::get('AlmacenConsumo', 'ConsumoController@AlmacenConsumo');
+Route::post('AlmacenConsumo', 'ConsumoController@AlmacenConsumo');
 
 //PARA EL ADMINISTRADOR GENERAL DE LA PAGINA
 Route::get('LoginAdmin', 'AdministradorController@LoginAdmin');

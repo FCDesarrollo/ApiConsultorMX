bc72d8288672fb72f93446cb09a199514335deed
Arturo Gallegos Ortega <jgallegosortega@gmail.com>
2020-02-10 17:05:32 -0700

    prueba empresas

--- miconsultor/app/Http/Controllers/AdministradorController.php
+++ miconsultor/app/Http/Controllers/AdministradorController.php
@@ -488,4 +488,23 @@ class AdministradorController extends Controller
         return $datos;
     }
 
+    public function listaempresas(Request $request)
+    {
+        $valida = $this->usuarioadmin($request->usuario, $request->pwd);
+        if ($valida != "2" and $valida != "3"){
+            $usuario = $valida['usuario'];
+            $iduser = $usuario[0]->idusuario;
+            $empresa = DB::connection("General")->select("SELECT mc1000.* FROM mc1002 m02 
+                                                    INNER JOIN mc1000 on m02.idempresa=mc1000.idempresa 
+                                                    WHERE m02.idusuario=$iduser AND mc1000.status=1");
+                $datos = array(
+                   "empresa" => $empresa,
+                );
+        }else{
+            $datos = $valida;
+        }
+        return json_encode($datos, JSON_UNESCAPED_UNICODE);
+        //return $datos;
+    }
+
 }

564cf6d4a71ab82053a76f10cb66a5064aeb5521
Arturo Gallegos Ortega <jgallegosortega@gmail.com>
2020-02-12 19:04:06 -0700

    Update MenuController.php

--- miconsultor/app/Http/Controllers/MenuController.php
+++ miconsultor/app/Http/Controllers/MenuController.php
@@ -16,12 +16,14 @@ class MenuController extends Controller
         if ($valida[0]['error'] == 0){
             $modulos = DB::connection("General")->select("SELECT * FROM mc1003");
             for ($i=0; $i < count($modulos); $i++) {
+                $idmodulo =  $modulos[$i]->idmodulo;
                 $menus = DB::connection("General")->select("SELECT * FROM mc1004 
-                                            WHERE idmodulo=$modulos[$i]->idmodulo");
+                                            WHERE idmodulo=$idmodulo");
                 $modulos[$i]->menus = $menus;
                 for ($x=0; $x < count($menus); $x++) {
+                    $idmenu = $menus[$x]->idmenu;
                     $submenus = DB::connection("General")->select("SELECT * FROM mc1005 
-                                    WHERE idmenu=$menus[$x]->idmenu");
+                                    WHERE idmenu=$idmenu");
                     $menus[$x]->submenus = $submenus;
                 }
                 $array["modulos"][$i] = $modulos[$i];

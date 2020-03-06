c6f4336871782d0e578debf30ce110c550b0da38
Arturo Gallegos Ortega <jgallegosortega@gmail.com>
2020-03-06 13:42:37 -0700

    Update AutorizacionyGastosController.php

--- miconsultor/app/Http/Controllers/AutorizacionyGastosController.php
+++ miconsultor/app/Http/Controllers/AutorizacionyGastosController.php
@@ -209,7 +209,7 @@ class AutorizacionyGastosController extends Controller
             $empresa = DB::connection("General")->select('select * from mc1000 where rfc = ?', [$request->rfc]);
             $bdd = $empresa[0]->rutaempresa;
             $query ="select r.*,s.sucursal,u.nombre,u.apellidop,u.apellidom from $bdd.mc_requerimientos r INNER JOIN " .env('DB_DATABASE_GENERAL').".mc1001 u ON r.id_usuario=u.idusuario 
-                            inner join $bdd.mc_mc_catsucursales s ON r.id_sucursal=s.idsucursal                     
+                            inner join $bdd.mc_catsucursales s ON r.id_sucursal=s.idsucursal                     
                                 where id_usuario =$idusuario and id_departamento=$request->idsubmenu";
             $requser = DB::select($query);
             
@@ -217,7 +217,7 @@ class AutorizacionyGastosController extends Controller
             for ($i=0; $i < count($conceptos); $i++) { 
                 $idconcepto = $conceptos[$i]->id_concepto;
                 $query ="select r.*,s.sucursal,u.nombre,u.apellidop,u.apellidom from $bdd.mc_requerimientos r INNER JOIN " .env('DB_DATABASE_GENERAL').".mc1001 u ON r.id_usuario=u.idusuario 
-                                inner join $bdd.mc_mc_catsucursales s ON r.id_sucursal=s.idsucursal    
+                                inner join $bdd.mc_catsucursales s ON r.id_sucursal=s.idsucursal    
                             where id_concepto =$idconcepto and id_usuario<>$idusuario and id_departamento=$request->idsubmenu";
                 $reqconceto = DB::select($query);
                 $requerimientos = array_merge($requser, $reqconceto);
